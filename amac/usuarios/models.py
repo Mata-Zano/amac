cb16dcef4d83c941ac14dc9e95a5037c9d6cdd66
Mata-Zano <cupelt@hotmail.com>
2024-10-27 21:05:21 -0300

    Modificacion Usuario y Rol

--- amac/usuarios/models.py
+++ amac/usuarios/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class roles (models.Model):
+
+
+
+class Roles (models.Model):
     nombre = models.CharField(max_length=30,  null= False, unique=True, verbose_name='Nombre')
 
     def __str__(self):
@@ -11,5 +15,7 @@ class roles (models.Model):
         verbose_name = 'Rol'
         verbose_name_plural = 'Roles'
 
+
+class User(AbstractUser):
+    rol = models.ForeignKey(Roles, null=False, blank=False, on_delete= models.DO_NOTHING)
 # Create your models here.
-    
