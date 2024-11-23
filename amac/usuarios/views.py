fd41388277788a39183b9ad741ea699d33ddc9c5
Fabian <fabianrojas2d@gmail.com>
2024-11-23 19:04:50 -0300

    CRUD Usuarios diseño y lógico mejorados

--- amac/usuarios/views.py
+++ amac/usuarios/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render,redirect
 from usuarios.models import User
 from usuarios.forms import FormUser
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
 
 
-
-def user (request):
+@login_required
+def user(request):
     usuarios = User.objects.all()
-    data = {'usuarios': usuarios}
-    return render(request, 'listado.html',data)
+    data = [
+        {
+            'id': usuario.id,
+            'nombre': f"{usuario.first_name} {usuario.last_name}",
+            'email': usuario.email,
+            'rol': usuario.rol.nombre if usuario.rol else "Sin asignar"
+        }
+        for usuario in usuarios
+    ]
+    return render(request, 'listado.html', {'usuarios': data})
 
+@login_required
 def addUser (request):
     form = FormUser()
     if request.method == 'POST' :
         form = FormUser(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Usuario creado correctamente.')
         return redirect('/')
     data = {'form' : form}
-    return render(request, 'agregar.html', data)   
-
+    return render(request, 'agregar.html', data)
+  
+@login_required
 def editUser(request, id):
     usuario = User.objects.get(id = id)
     form = FormUser(instance=usuario)
@@ -26,6 +42,7 @@ def editUser(request, id):
         form = FormUser(request.POST, instance=usuario)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuario editado correctamente.')
         return redirect('/')
     data = {'form':form}
     return render(request, 'agregar.html', data)
@@ -33,6 +50,7 @@ def editUser(request, id):
 def deleteUser(request, id):
     usuario = User.objects.get(id = id)
     usuario.delete()
+    messages.success(request, 'Usuario eliminado correctamente.')
     return redirect('/')
 
 # Create your views here.
