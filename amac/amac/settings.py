c92ff65cec644a7142c66588b176d0ed2f62e349
Mata-Zano <cupelt@hotmail.com>
2024-11-24 16:35:58 -0300

    heroku 2

--- amac/amac/settings.py
+++ amac/amac/settings.py
@@ -126,11 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # VARIABLE DE REDIRECION DE LOGIN Y LOGOUT
 
@@ -138,3 +142,5 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 AUTH_USER_MODEL = 'usuarios.User'
+
+print(os.path.join(BASE_DIR, 'static'))
